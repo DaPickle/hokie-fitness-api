7b023d2b147746b9f1b9c6bfa26204ede8763fbe
DaPickle <dillon.a.rose@gmail.com>
2024-03-24 21:31:35 -0400

    update ip

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,15 @@
 use std::env;
 
 pub struct Config {
-    pub ip_address: String,
+    pub port: u16,
 }
 
 impl Config {
     pub fn new() -> Self {
-        let ip = env::var("IP").unwrap_or(String::from("127.0.0.1"));
-        let port = env::var("PORT").unwrap_or(String::from("8080"));
+        let port = env::var("PORT").unwrap_or(String::from("8080")).parse::<u16>().unwrap_or(8080);
 
         Self {
-            ip_address: format!("{ip}:{port}")
+            port
         }
     }
 }
